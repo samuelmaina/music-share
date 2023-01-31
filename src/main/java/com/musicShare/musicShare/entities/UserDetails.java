8e0ccb01e6cb9ccab97e181e76d8bc5d951fe91d
samuelmayna <samuelmayna@gmail.com>
2023-01-31 14:46:31 +0300

    Changed name since user is a keyword in the H2 database

---
+++ src/main/java/com/musicShare/musicShare/entities/UserDetails.java
@@ -0,0 +1,62 @@
+package com.musicShare.musicShare.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private boolean enabled;
+
+    public UserDetails(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.enabled = false;
+    }
+
+    public UserDetails() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
---
+++ src/test/java/com/musicShare/musicShare/repositories/UserDetailsRepositoryTests.java
@@ -0,0 +1,34 @@
+package com.musicShare.musicShare.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.musicShare.musicShare.entities.UserDetails;
+
+@DataJpaTest
+public class UserDetailsRepositoryTests {
+
+    @Autowired
+    private UserDetailsRepository repository;
+
+    @Test
+    public void testFindByEmail() {
+
+        UserDetails Account1 = new UserDetails("Account1@test.com", "password1");
+        UserDetails Account2 = new UserDetails("Account2@test.com", "password2");
+        UserDetails Account3 = new UserDetails("Account3@test.com", "password3");
+
+        System.out.println(Account1);
+        repository.save(Account1);
+        repository.save(Account2);
+        repository.save(Account3);
+
+        UserDetails Account = repository.findByEmail("Account1@test.com");
+        assertEquals(Account.getEmail(), Account1.getEmail());
+    }
+
+}
