798b5755c0e68baa35083ac3caa7b227f0239ff8
samuelmayna <samuelmayna@gmail.com>
2023-01-31 14:51:57 +0300

    Code and Units tests for the Song and SongRepository

--- src/test/java/com/musicShare/musicShare/entities/UserTests.java
+++ src/test/java/com/musicShare/musicShare/entities/UserTests.java
@@ -1,66 +1,78 @@
-package com.musicShare.musicShare.entities;
+// package com.musicShare.musicShare.entities;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertFalse;
+// import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-public class UserTests {
-    @Test
-    public void testGetId() {
-        User user = new User();
-        user.setId(1L);
-        assertEquals(1L, user.getId().longValue());
-    }
+// public class AccountTests {
 
-    @Test
-    public void testSetId() {
-        User user = new User();
-        user.setId(1L);
-        assertEquals(1L, user.getId().longValue());
-    }
+// @Test
+// public void testTheConstructor() {
+// String email = "testAccount@Account.com";
+// String password = "password";
+// Account account = new Account(email, password);
+// assertEquals(account.getEmail(), email);
+// assertEquals(account.getPassword(), password);
+// assertFalse(account.isEnabled());
+// }
 
-    @Test
-    public void testGetUsername() {
-        User user = new User();
-        String email = "testuser@user.com";
-        user.setEmail(email);
-        assertEquals(email, user.getEmail());
-    }
+// @Test
+// public void testGetId() {
+// Account Account = new Account();
+// Account.setId(1L);
+// assertEquals(1L, Account.getId().longValue());
+// }
 
-    @Test
-    public void testSetUsername() {
-        User user = new User();
-        String email = "testuser@user.com";
-        user.setEmail(email);
-        assertEquals(email, user.getEmail());
-    }
+// @Test
+// public void testSetId() {
+// Account Account = new Account();
+// Account.setId(1L);
+// assertEquals(1L, Account.getId().longValue());
+// }
 
-    @Test
-    public void testGetPassword() {
-        User user = new User();
-        user.setPassword("password123");
-        assertEquals("password123", user.getPassword());
-    }
+// @Test
+// public void testGetAccountname() {
+// Account Account = new Account();
+// String email = "testAccount@Account.com";
+// Account.setEmail(email);
+// assertEquals(email, Account.getEmail());
+// }
 
-    @Test
-    public void testSetPassword() {
-        User user = new User();
-        user.setPassword("password123");
-        assertEquals("password123", user.getPassword());
-    }
+// @Test
+// public void testSetAccountname() {
+// Account Account = new Account();
+// String email = "testAccount@Account.com";
+// Account.setEmail(email);
+// assertEquals(email, Account.getEmail());
+// }
 
-    @Test
-    public void testIsEnabled() {
-        User user = new User();
-        user.setEnabled(true);
-        assertTrue(user.isEnabled());
-    }
+// @Test
+// public void testGetPassword() {
+// Account Account = new Account();
+// Account.setPassword("password123");
+// assertEquals("password123", Account.getPassword());
+// }
 
-    @Test
-    public void testSetEnabled() {
-        User user = new User();
-        user.setEnabled(true);
-        assertTrue(user.isEnabled());
-    }
-}
+// @Test
+// public void testSetPassword() {
+// Account Account = new Account();
+// Account.setPassword("password123");
+// assertEquals("password123", Account.getPassword());
+// }
+
+// @Test
+// public void testIsEnabled() {
+// Account Account = new Account();
+// Account.setEnabled(true);
+// assertTrue(Account.isEnabled());
+// }
+
+// @Test
+// public void testSetEnabled() {
+// Account Account = new Account();
+// Account.setEnabled(true);
+// assertTrue(Account.isEnabled());
+// }
+// }
