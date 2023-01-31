20d2ce6fb4da4ae389ca8775c31827e6709ae010
samuelmayna <samuelmayna@gmail.com>
2023-01-31 09:47:46 +0300

    Added song and user classes together with their tests

---
+++ src/test/java/com/musicShare/musicShare/models/UserTests.java
@@ -0,0 +1,64 @@
+package com.musicShare.musicShare.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTests {
+    @Test
+    public void testGetId() {
+        User user = new User();
+        user.setId(1L);
+        assertEquals(1L, user.getId().longValue());
+    }
+
+    @Test
+    public void testSetId() {
+        User user = new User();
+        user.setId(1L);
+        assertEquals(1L, user.getId().longValue());
+    }
+
+    @Test
+    public void testGetUsername() {
+        User user = new User();
+        user.setUsername("testuser");
+        assertEquals("testuser", user.getUsername());
+    }
+
+    @Test
+    public void testSetUsername() {
+        User user = new User();
+        user.setUsername("testuser");
+        assertEquals("testuser", user.getUsername());
+    }
+
+    @Test
+    public void testGetPassword() {
+        User user = new User();
+        user.setPassword("password123");
+        assertEquals("password123", user.getPassword());
+    }
+
+    @Test
+    public void testSetPassword() {
+        User user = new User();
+        user.setPassword("password123");
+        assertEquals("password123", user.getPassword());
+    }
+
+    @Test
+    public void testIsEnabled() {
+        User user = new User();
+        user.setEnabled(true);
+        assertTrue(user.isEnabled());
+    }
+
+    @Test
+    public void testSetEnabled() {
+        User user = new User();
+        user.setEnabled(true);
+        assertTrue(user.isEnabled());
+    }
+}
