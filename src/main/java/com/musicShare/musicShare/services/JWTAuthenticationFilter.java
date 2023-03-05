3fcfb83b6b26be627a8a99e5083d4dab22d175c4
samuelmayna <samuelmayna@gmail.com>
2023-03-05 08:37:35 +0300

    Moved the secret and the expiry time to env. vals

--- src/main/java/com/musicShare/musicShare/services/JWTAuthenticationFilter.java
+++ src/main/java/com/musicShare/musicShare/services/JWTAuthenticationFilter.java
@@ -20,7 +20,6 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class JWTAuthenticationFilter extends BasicAuthenticationFilter {
@@ -28,7 +27,11 @@ public class JWTAuthenticationFilter extends BasicAuthenticationFilter {
     private final UserDetailsService userDetailsService;
 
     @Value("${jwt.secret}")
+
     private String jwtSecret;
+
+    @Value("${jwt.expiration-hours}")
+    long jwtExpirationInHours;
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
         super(authenticationManager);
@@ -57,13 +60,15 @@ public class JWTAuthenticationFilter extends BasicAuthenticationFilter {
     }
 
     private boolean validateToken(String token) {
+
         try {
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
             return true;
         } catch (Exception e) {
             logger.error("Error validating JWT token", e);
+            return false; // return false when token is invalid
         }
-        return false;
+
     }
 
     public Authentication getAuthentication(String token) {
@@ -72,9 +77,9 @@ public class JWTAuthenticationFilter extends BasicAuthenticationFilter {
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 
-    public static String generateToken(UserDetails userDetails, String jwtSecret, long jwtExpiration) {
+    public String generateToken(UserDetails userDetails) {
         Date now = new Date();
-        Date expirationDate = new Date(now.getTime() + jwtExpiration);
+        Date expirationDate = new Date(now.getTime() + jwtExpirationInHours * 3600);
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(now)
