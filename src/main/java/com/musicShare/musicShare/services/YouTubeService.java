f435e1899e3b4568b700ad09a4fb997a7a25ad47
samuelmayna <samuelmayna@gmail.com>
2023-03-13 16:28:10 +0300

    Added search service to youtube and its tests

---
+++ src/main/java/com/musicShare/musicShare/services/YouTubeService.java
@@ -0,0 +1,42 @@
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class YouTubeService {
+    @Value("${youtube.APP_NAME}")
+    private static final String APPLICATION_NAME;
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final long MAX_RESULTS = 10L; // maximum number of search results to retrieve
+
+    private final YouTube youTube;
+
+    public YouTubeService() throws GeneralSecurityException, IOException {
+        this.youTube = new YouTube.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JSON_FACTORY,
+                null)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public List<SearchResult> searchSongs(String phrase) throws IOException {
+        YouTube.Search.List searchList = youTube.search()
+                .list(List.of("id", "snippet")) // include id and snippet data in the results
+                .setMaxResults(MAX_RESULTS)
+                .setType("video") // only search for videos
+                .setFields("items(id(videoId),snippet(title,channelTitle))")
+                .setQ(phrase); // search phrase
+
+        SearchListResponse response = searchList.execute();
+        return response.getItems();
+    }
+}
