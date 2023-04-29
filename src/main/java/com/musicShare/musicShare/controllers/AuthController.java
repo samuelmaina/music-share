b81b78df4a483ec0f10ffdde5e9e2a99fa484d71
samuelmayna <samuelmayna@gmail.com>
2023-04-29 10:47:55 +0300

    Refactored AuthController: Removed  the JWT logic and put eveyrhing under "auth"url

--- src/main/java/com/musicShare/musicShare/controllers/AuthController.java
+++ src/main/java/com/musicShare/musicShare/controllers/AuthController.java
@@ -1,31 +1,24 @@
 package com.musicShare.musicShare.controllers;
 
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.musicShare.musicShare.entities.UserDetails;
 import com.musicShare.musicShare.repositories.UserDetailsRepository;
-import com.musicShare.musicShare.services.JWTAuthenticationFilter;
+import com.musicShare.musicShare.services.controllers.JWTAuthenticationFilter;
 
 @RestController
+@RequestMapping(value = "/auth")
 public class AuthController {
     @Autowired
     private UserDetailsRepository userDetailsRepository;
     @Autowired
     private JWTAuthenticationFilter jwtAuthenticationFilter;
-
-    public String hash(String password) {
-        return BCrypt.hashpw(password, BCrypt.gensalt(12));
-    }
-
-    public boolean verifyHash(String password, String hash) {
-        return BCrypt.checkpw(password, hash);
-    }
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody UserDetails user) {
@@ -33,16 +26,14 @@ public class AuthController {
             return new ResponseEntity<>("Email already exists",
                     HttpStatus.BAD_REQUEST);
         }
-
-        user.setPassword(hash(user.getPassword()));
-        user.setEnabled(true);
+        user.hashPassword();
         userDetailsRepository.save(user);
-
         return new ResponseEntity<>("Account created Successfully.", HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserDetails user) {
+
         UserDetails foundUser = userDetailsRepository.findByEmail(user.getEmail());
         String errorMessage = "Invalid email or password";
 
@@ -50,7 +41,7 @@ public class AuthController {
             return new ResponseEntity<>(errorMessage, HttpStatus.UNAUTHORIZED);
         }
 
-        if (!verifyHash(user.getPassword(), foundUser.getPassword())) {
+        if (!foundUser.confirmPassword(user.getPassword())) {
             return new ResponseEntity<>(errorMessage, HttpStatus.UNAUTHORIZED);
         }
 
--- src/test/java/com/musicShare/musicShare/Controllers/AuthControllerTests.java
+++ src/test/java/com/musicShare/musicShare/Controllers/AuthControllerTests.java
@@ -1,83 +1,136 @@
-package com.musicShare.musicShare.Controllers;
+package com.musicShare.musicShare.controllers;
 
-import static org.junit.Assert.assertEquals;
+import com.musicShare.musicShare.services.controllers.JWTAuthenticationFilter;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.musicShare.musicShare.controllers.AuthController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.musicShare.musicShare.entities.UserDetails;
 import com.musicShare.musicShare.repositories.UserDetailsRepository;
 
-@RunWith(MockitoJUnitRunner.class)
-public class AuthControllerTests {
+class AuthControllerTests {
+    private MockMvc mockMvc;
+
+    private final String baseUrl = "/auth";
 
     @Mock
     private UserDetailsRepository userDetailsRepository;
 
+    @Mock
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+
     @InjectMocks
     private AuthController authController;
-    private UserDetails user;
 
-    private String testEmail = "testUserDetails@test.com";
-
-    @Before
-    public void setUp() {
-        user = new UserDetails();
-        user.setEmail(testEmail);
-        user.setPassword("password");
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
     }
-
-@Test
-public void signup_existingUserDetailsname_returnsBadRequest() {
-when(userDetailsRepository.findByEmail(testEmail)).thenReturn(new
-UserDetails(testEmail,"password"));
-
-ResponseEntity<?> response = authController.signup(user);
-
-assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-assertEquals("Email already exists", response.getBody());
-}
-
-@Test
-public void signup_newUserDetails_returnsCreated() {
-when(userDetailsRepository.findByEmail(testEmail)).thenReturn(null);
-when(userDetailsRepository.save(any(UserDetails.class))).thenReturn(new UserDetails());
-
-ResponseEntity<?> response = authController.signup(user);
-
-assertEquals(HttpStatus.CREATED, response.getStatusCode());
-assertEquals("Account created Successfully.", response.getBody());
-}
-
-@Test
-public void login_invalidUserDetailsname_returnsUnauthorized() {
-when(userDetailsRepository.findByEmail(testEmail)).thenReturn(null);
-
-ResponseEntity<?> response = authController.login(user);
-
-assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-assertEquals("Invalid email or password", response.getBody());
-}
 
     @Test
-    public void login_invalidPassword_returnsUnauthorized() {
-        UserDetails foundUserDetails = new UserDetails();
-        foundUserDetails.setPassword(authController.hash("incorrectPassword"));
-        when(userDetailsRepository.findByEmail(testEmail)).thenReturn(foundUserDetails);
+    void testSignupWithValidUser() throws Exception {
+        UserDetails user = new UserDetails();
+        user.setEmail("user@example.com");
+        user.setPassword("password");
 
-        ResponseEntity<?> response = authController.login(user);
+        when(userDetailsRepository.findByEmail(user.getEmail())).thenReturn(null);
+        when(userDetailsRepository.save(any(UserDetails.class))).thenReturn(user);
 
-        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-        assertEquals("Invalid email or password", response.getBody());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post(baseUrl + "/signup").contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isCreated())
+                .andExpect(content().string("Account created Successfully."));
+
     }
 
+    @Test
+    void testSignupWithExistingEmail() throws Exception {
+        UserDetails user = new UserDetails();
+        user.setEmail("user@example.com");
+        user.setPassword("password");
+
+        when(userDetailsRepository.findByEmail(user.getEmail())).thenReturn(user);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post(baseUrl + "/signup").contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("Email already exists"));
+    }
+
+    @Test
+    void testLoginWithValidUser() throws Exception {
+        UserDetails user = new UserDetails();
+        user.setEmail("user@example.com");
+        user.setPassword("password");
+
+        UserDetails hashedUser = new UserDetails();
+        hashedUser.setEmail("user@example.com");
+        hashedUser.setPassword("password");
+        hashedUser.hashPassword();
+
+        when(userDetailsRepository.findByEmail(hashedUser.getEmail())).thenReturn(hashedUser);
+        when(jwtAuthenticationFilter.generateToken(hashedUser)).thenReturn("token");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post(baseUrl + "/login").contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andExpect(content().string("token"));
+    }
+
+    @Test
+    void testLoginWithInvalidEmail() throws Exception {
+        UserDetails user = new UserDetails();
+        user.setEmail("user@example.com");
+        user.setPassword("password");
+        user.setEnabled(true);
+
+        when(userDetailsRepository.findByEmail(user.getEmail())).thenReturn(null);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post(baseUrl + "/login").contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string("Invalid email or password"));
+    }
+
+    @Test
+    void testLoginWithInvalidPassword() throws Exception {
+        UserDetails user = new UserDetails();
+        user.setEmail("user@example.com");
+        user.setPassword("password");
+        user.setEnabled(true);
+
+        when(userDetailsRepository.findByEmail(user.getPassword())).thenReturn(null);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post(baseUrl + "/login").contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string("Invalid email or password"));
+    }
 }
