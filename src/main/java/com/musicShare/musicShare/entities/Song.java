e67ea43e1cfd7b554cd1597654e1894bb6c107b4
samuelmayna <samuelmayna@gmail.com>
2023-05-12 17:00:03 +0300

    Modification: Added the OneToOne rel in Songs entity

--- src/main/java/com/musicShare/musicShare/entities/Song.java
+++ src/main/java/com/musicShare/musicShare/entities/Song.java
@@ -2,9 +2,12 @@ package com.musicShare.musicShare.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Song {
@@ -36,6 +39,14 @@ public class Song {
         return this.songUrl;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
@@ -59,4 +70,5 @@ public class Song {
     public void setViews(Long views) {
         this.views = views;
     }
+
 }
